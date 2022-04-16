29454591777239b4a542bdf6762025c7b63ba8bb
AvinashShuka007 <visshukla75@gmail.com>
2022-04-16 23:15:17 +0530

    bestseller

---
+++ bestseller.js
@@ -0,0 +1,152 @@
+var bestsellersData = [
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/AAWA125-AATN125-AATT030-AAPM030-REDEFINE-Regimen-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3wzMTE2NXxpbWFnZS9wbmd8aW1hZ2VzL2hkNy9oMWMvMTM3NTgzODQwNzg4NzgucG5nfDE5NTM4Yzc1OTA0MWNiY2M5NzYwMDZhMTQ0NGVmZDEzZmRhYzVlZWE4YWRhODk3NWQ0ZGU1NDlhNzVjZTUyOTU"
+       ,
+       name: "REDEFINE REGIMEN",
+       price: "220.00 | $197.00 With perk",
+       button: "ADD TO BAG",
+    },
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/TRFS-Regimen-Explainer-GLOBAL-570x570.png?context=bWFzdGVyfGltYWdlc3wyODM2M3xpbWFnZS9wbmd8aW1hZ2VzL2g5Yy9oNzcvMTM3NTgzODUzODk1OTgucG5nfGYyZGUwNzdmOWM3N2Q2M2FlMjM4Zjc4MjQxNWZkNmIzOTc0NjIyZWM3MDgyZTY3YzA5YjMyZjNhZWY3MGE3YzY"
+       ,
+       name: "Total RF Serum",
+       price: "175.00 | $157.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/ENHLSH01-Regimen-Explainer-R-F-Lash-Boost-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wxMDc0OHxpbWFnZS9wbmd8aW1hZ2VzL2hmMS9oYWMvMTQ1MzIwNDI1ODgxOTAucG5nfDQyNTQ0OGUzYmMwZTJkM2YzNzE1MjE2NzQ1M2YxMWQ5M2E3ZWQ2MjAwMDA3MzFmNjQyOWUwNDU3OWZmMTkzZDY"
+       ,
+       name: "R + F Lash Boost",
+       price: "155.00 | $139.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/ENHEY15-720x600-DESKTOP-v4.jpg?context=bWFzdGVyfGltYWdlc3w2MDYyOXxpbWFnZS9qcGVnfGltYWdlcy9oZDcvaGI2LzEzNTMzMTczODc0NzE4LmpwZ3w0MGI1MmM0OTExMmRiMzUzYzg5YTIyNWJjZTdiOTZkNzk1ZTVmOTk2MTBhY2IzZTcwNzhkZDI4NTUxZDI5Zjhh"
+       ,
+       name: "Active Hydration Bright Eye Complex",
+       price: "$72.00 | $64.00 WithPC PERKS",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/ENHEFG1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyMjI3N3xpbWFnZS9wbmd8aW1hZ2VzL2hkMS9oM2MvMTQ1MzIxNjY2MTUwNzAucG5nfGZlMDQ0NDRlMjczNDFlZjdkYWVmNjkwZWY4MDAwM2I4MjMwZGZiMzU3ZTViZGNlYjllNzBiZjAwY2FlZDU5NzA"
+       ,
+       name: "Active Hydration Serum",
+       price: "112.00 | $100.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/MFEC-Regimen-Explainer-GLOBAL-570x570.png?context=bWFzdGVyfGltYWdlc3wyMzc3NHxpbWFnZS9wbmd8aW1hZ2VzL2gzNS9oZmMvMTM3NTgzODU0NTUxMzQucG5nfDNmYWYyNzdiZGY5OWIyY2UzMDYyZDliYmM2YmUzMTRmOGY3Yzg0YmY1ZjY2YjgyYWU1OGNhZDZiN2IzMjViZGI"
+       ,
+       name: "REDEFINE Multi-Function Eye Cream",
+       price: "70.00 | $63.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/ENBR001-720x600-DESKTOP.jpg?context=bWFzdGVyfHJvb3R8MTg2NDR8aW1hZ2UvanBlZ3xoNWMvaGQ5LzEzNTMzMTYzMDk0MDQ2LmpwZ3xiNmNjNjE3YmJmOTI4Y2UzZjAwM2RkMDQwZDNkNmNiZDQyZWQzMzQ4N2ZjZmMxOWY1OGEzYjQ1MTFiM2M1MGE5"
+       ,
+       name: "Rodan + Fields Active Hydration Body...",
+       price: "66.00 | $59.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/ENPS125-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wxODU1NXxpbWFnZS9wbmd8aW1hZ2VzL2gyNi9oZjgvMTQ1MzIxNjc2MzA4NzgucG5nfGM0YTc0NjM0YWIzM2QxZDZlOTc0NTdmYjA4M2EyNDA5NGJhODg3ZDQwMGJjZjdiMTZlYTI2MjYwNjI2Y2JhZGE"
+       ,
+       name: "Rodan + Fields Enhancements Micro-...",
+       price: "83.00 | $74.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/RVWA125-RVTG125-RVSS050-RVTTG50-REVERSE-Regimen-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3w0MTY5NHxpbWFnZS9wbmd8aW1hZ2VzL2gyNy9oZWIvMTM3NTgzODQzNDEwMjIucG5nfGI3NmIzMjI0NTc2Y2RiZjZlNTdhYjg0ZDM4ZjM0MmM2M2U5Yjg5Y2RiNTM1NTM1NGFlOWNiY2E5ZTVkYTM5NWY"
+       ,
+       name: "REVERSE Regimen",
+       price: "195.00 | $175.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/UNWA125-UNTJ125-UNTT030-UNSS030-UNBLEMISH-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3w1NDk5N3xpbWFnZS9wbmd8aW1hZ2VzL2g2YS9oOTYvMTM3NTgzODQ4NjUzMTAucG5nfDNhOWMxMGU5MjIzMGEyZjNhNTZjNWVkMjUxYmJjODI5YmNlODhjODBkNzJjZTA4ZDk1YTk0Yzk0ZjkyNzdhZmU"
+       ,
+       name: "UNBLEMISH Regimen",
+       price: "190.00 | $171.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/SOWA125-SOTT050-SOMO050-SOSS050-SOOTHE-Regimen-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3w0MDc1MHxpbWFnZS9wbmd8aW1hZ2VzL2g5Zi9oYWMvMTM3NTgzODUxMjc0NTQucG5nfGFmOTE5ZGY1MjdhZmFkNDhhNzNiOTkyYTYzODAxNzI4OTE3ZGQ5Zjk3YzRlOGRlNTIzYzU3MzZlMTQzNGEzNTI"
+       ,
+       name: "SOOTHE Regimen",
+       price: "185.00 | $166.00 With perk",
+       button: "ADD TO BAG",
+    },
+
+    {
+       image_url:
+          "https://www.rodanandfields.com/en-us/medias/RCWA125-RCTTO50-RCSP040-REGHARGE-Regimen-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3wyNTQ5NHxpbWFnZS9wbmd8aW1hZ2VzL2hkNi9oNDMvMTM3NTgzODQ2MDMxNjYucG5nfGQ4MzYxODU0ZDcxOTZiOTdlODg3NmZmZTAzZGQ1MGQ5Yjg0YzBhODk3NzJhYzM5NzZlNjQ1NjQzNGQ3NTY0ZmE"
+       ,
+       name: "RECHARGE Regimen",
+       price: "134.00 | $120.00 With perk",
+       button: "ADD TO BAG",
+    },
+ ];
+
+
+var cartData ;
+   if(localStorage.getItem("cartData")==null){
+      cartData = [];
+   }else{
+      cartData = JSON.parse(localStorage.getItem('cartData'))
+   }
+
+   bestsellersData.forEach(function (bestsellerData) {
+
+      var box = document.createElement('div');
+
+      var img = document.createElement('img');
+      img.src = bestsellerData.image_url;
+
+      var name = document.createElement('p');
+      name.textContent = bestsellerData.name;
+
+      var price = document.createElement('price');
+      price.textContent = `$${bestsellerData.price}`;
+
+      var button = document.createElement('button');
+      // cartButton.innerText = "ADD TO CART";
+      button.innerText = bestsellerData.button;
+       
+
+
+      button.addEventListener('click', function () {
+         addtoCart(bestsellerData)
+
+      });
+
+      box.append(img, name, price, button);
+
+      document.querySelector('#container').append(box)
+
+   });
+   
+   function addtoCart(product){
+      cartData.push(product);
+      console.log(cartData);
+      localStorage.setItem('cartData',JSON.stringify(cartData));
+   }
